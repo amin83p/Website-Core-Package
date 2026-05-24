c7ce5bafb1563384bc07a5388a605e6bff9182f3
amin83p <paknejad@live.com>
2026-05-24 00:38:39 -0600

    feat(pte): split route dependency core adapter

--- packages/pte/MVC/services/pte/pteRouteDependencies.js
+++ packages/pte/MVC/services/pte/pteRouteDependencies.js
@@ -1,12 +1,1 @@
-const { requireAuth } = require('../../../../MVC/middleware/authMiddleware');
-const { requireAccess } = require('../../../../MVC/middleware/accessMiddleware');
-const { trackActionState } = require('../../../../MVC/middleware/actionStateMiddleware');
-const { SECTIONS, OPERATIONS } = require('../../../../config/accessConstants');
-
-module.exports = {
-  requireAuth,
-  requireAccess,
-  trackActionState,
-  SECTIONS,
-  OPERATIONS
-};
+module.exports = require('./pteRouteCoreDependencies');
