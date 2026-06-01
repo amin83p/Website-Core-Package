13a5793b8cc7639d3d828c3bacca65d7e25d8a15
amin83p <paknejad@live.com>
2026-05-31 22:29:19 -0600

    fix(pte-package): stabilize core resolver import path for runtime mount
    - extract core-module resolution into dedicated pteCoreModuleResolver
    
    - update pteCoreContracts and utility wrappers to import resolver directly
    
    - prevent startup/runtime mount failures caused by init-order resolver access

--- packages/pte/MVC/utils/encyptors.js
+++ packages/pte/MVC/utils/encyptors.js
@@ -1,3 +1,3 @@
-const { requireCoreModule } = require('../services/pte/pteCoreContracts');
+const { requireCoreModule } = require('../services/pte/pteCoreModuleResolver');
 
 module.exports = requireCoreModule('MVC/utils/encyptors');
