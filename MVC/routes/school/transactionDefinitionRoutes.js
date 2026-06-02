87e457d232daf133b9144375897a228e0b43f632
amin83p <paknejad@live.com>
2026-06-01 21:56:35 -0600

    chore: pass31 prune core school domain artifacts

--- MVC/routes/school/transactionDefinitionRoutes.js
+++
@@ -1,3 +0,0 @@
-// Backward compatible route alias:
-// `/school/transactionDefinitions` now uses the same router as `/school/transactionTemplates`.
-module.exports = require('./transactionTemplateRoutes');
